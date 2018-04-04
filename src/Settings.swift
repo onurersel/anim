2becf6de16b35a29eb5e476f42f1fd7e0af66f3b
Teng Foung <tengfoung@live.com>
2018-04-04 15:59:03 +0800

    Xcode 9.3 fix.

--- src/Settings.swift
+++ src/Settings.swift
@@ -9,18 +9,24 @@ import Foundation
 
 /// Animation settings.
 public struct animSettings {
-
+    init(delay: TimeInterval = 0, duration: TimeInterval = 1, ease: animEase = .easeOutQuint, completion: (animClosure)? = nil, isUserInteractionsEnabled: Bool = false) {
+        self.delay = delay
+        self.duration = duration
+        self.ease = ease
+        self.completion = completion
+        self.isUserInteractionsEnabled = isUserInteractionsEnabled
+    }
     /// Delay before animation starts.
-    public var delay: TimeInterval = 0
+    public var delay: TimeInterval
     /// Duration of animation.
-    public var duration: TimeInterval = 1
+    public var duration: TimeInterval
     /// Easing of animation.
-    public var ease: animEase = .easeOutQuint
+    public var ease: animEase
     /// Completion block which runs after animation.
     public var completion: (animClosure)?
     /// Preferred animator used for the animation.
     lazy public var preferredAnimator: AnimatorType = AnimatorType.default
     /// Enables user interactions on views while animating. Not available on macOS.
-    public var isUserInteractionsEnabled: Bool = false
+    public var isUserInteractionsEnabled: Bool
 
 }
